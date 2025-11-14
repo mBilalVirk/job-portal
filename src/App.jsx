1e943fd6945ed05b84956ff2ae3a4e319c0ae538
mBilalVirk <bilalvirk337@gmail.com>
2025-11-14 09:58:12 +0500

    companies miner update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import SearchJobs from "./components/searchjob/SearchJobs";
 import Jobs from "./components/jobs/Jobs";
+import Companies from "./components/companies/companies";
 import Footer from "./components/footer/Footer";
 import "./App.css";
-function App() {
-  const [count, setCount] = useState(0);
 
+function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <SearchJobs />
-      <section id="recruiters">
-        <div className="container text-center">
-          <h3>TOP RECRUITER</h3>
-        </div>
-      </section>
-      <Jobs />
+
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchJobs />
+
+              <section id="recruiters">
+                <div className="container text-center">
+                  <h3>TOP RECRUITER</h3>
+                </div>
+              </section>
+
+              <Jobs />
+            </>
+          }
+        />
+
+        {/* Companies Page */}
+        <Route path="/companies" element={<Companies />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/components/companies/companies.jsx
@@ -0,0 +1,18 @@
+function Companies() {
+  return (
+    <section id="companies">
+      <div className="container text-center">
+        <h3>Top Companies</h3>
+
+        <div className="companies-list">
+          <div className="company-card">Google</div>
+          <div className="company-card">Microsoft</div>
+          <div className="company-card">Amazon</div>
+          <div className="company-card">Meta</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Companies;
